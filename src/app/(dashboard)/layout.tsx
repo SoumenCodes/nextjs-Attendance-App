75e80a384544d54f819764a982f7136fa50d2825
SoumenCodes <soumen19j@gmail.com>
2024-12-22 13:27:35 +0530

    menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-// import "./globals.css";
+import Menu from "@/components/Menu";
+import Image from "next/image";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
-};
-
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>dashboard layout{children}</body>
-    </html>
+    <div className={"h-screen flex "}>
+      {/* left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+        <Link
+          href="/"
+          className="flex justify-center lg:justify-start items-center gap-2"
+        >
+          <Image src="/logo.png" alt="logo" height={30} width={32} />
+          <span className="hidden lg:block">SchoolRecord</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">R</div>
+    </div>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -112,3 +112,31 @@ const menuItems = [
     ],
   },
 ];
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Menu() {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 my-4">{i.title}</span>
+          {i.items.map((items) => (
+            <Link
+              href={items.href}
+              key={items.label}
+              className="flex items-center justify-center lg:justify-start gap-4 py-2 text-gray-400"
+            >
+              <Image src={items.icon} alt="icon" width={20} height={20} />
+              <span className="hidden lg:block">{items.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Menu;
