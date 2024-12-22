16e22527aa4e2f53cbe50a5bf3dfca2c7775caab
SoumenCodes <soumen19j@gmail.com>
2024-12-22 13:54:10 +0530

    navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import NavBar from "@/components/NavBar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,7 +22,10 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">R</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <NavBar />
+        {children}
+      </div>
     </div>
   );
 }
